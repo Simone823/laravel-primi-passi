875303bd31022e35dc625ba6c2ab0f583c13c8fe
Simone823 <simone.daglio823@outlook.it>
2022-04-11 17:49:26 +0200

    aggiunta header home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Primi passi</title>
+        <title>Home</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -17,12 +17,89 @@
                 margin: 0;
                 padding: 0;
                 box-sizing: border-box;
+                height: 100vh;
+                display: flex;
+                flex-direction: column;
+            }
+
+            header {
+                /* border: 3px solid red; */
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: rgb(31, 31, 31);
+                padding: 10px 0;
+            }
+
+            .menu {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                gap: 55px;
+                list-style: none;
+            }
+
+            a {
+                text-decoration: none;
+                text-transform: uppercase;
+                font-weight: 600;
+                color: #fff;
+            }
+
+            a:hover {
+                color: greenyellow;
+                transition: color 300ms ease-in-out;
+            }
+
+            main {
+                flex-grow: 1;
+                background-color: rgb(70, 70, 70);
+                padding: 20px 25px;
+            }
+
+            main h1 {
+                color: white;
+                text-align: center;
+            }
+
+            .active {
+                color: greenyellow;
             }
         </style>
     </head>
     <body>
+        
+        {{-- Header --}}
+        <header>
 
-        <h1>Hello World!</h1>
+            {{-- Menu --}}
+            <ul class="menu">
+                <li>
+                    <a class="active" href="{{route('Home')}}">Home</a>
+                </li>
+
+                <li>
+                    <a href="http://">About me</a>
+                </li>
+
+                <li>
+                    <a href="http://">Portfolio</a>
+                </li>
+
+                <li>
+                    <a href="http://">Contact</a>
+                </li>
+            </ul>
+
+        </header>
+
+        {{-- Main --}}
+        <main>
+            {{-- Title --}}
+            <h1>{{$title}}</h1>
+        </main>
+
+
 
 
     </body>
