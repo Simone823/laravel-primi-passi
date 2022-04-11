9432d07cc0d7155f56056d0851c07374a3d32744
Simone823 <simone.daglio823@outlook.it>
2022-04-11 18:39:59 +0200

    flex wrap menu home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -33,6 +33,7 @@
 
             .menu {
                 display: flex;
+                flex-wrap: wrap;
                 align-items: center;
                 justify-content: center;
                 gap: 55px;
