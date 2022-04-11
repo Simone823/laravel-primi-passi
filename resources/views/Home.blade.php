4fe55f34705bda3efc09147609a958f4fcdd5a64
Simone823 <simone.daglio823@outlook.it>
2022-04-11 17:05:20 +0200

    route web.php, delete welcome, add Home

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Primi passi</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            body {
+                background-color: #fff;
+                font-family: 'Nunito', sans-serif;
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+        </style>
+    </head>
+    <body>
+
+        <h1>Hello World!</h1>
+
+
+    </body>
+</html>
